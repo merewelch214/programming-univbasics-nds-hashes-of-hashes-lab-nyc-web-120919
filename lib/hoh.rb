357dafd5c910e56277faf1d8dec830691ddf6e5b
comical-sandbox-7108 <merewelch@gmail.com>
2019-11-05 01:40:49 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,7 +32,7 @@
 
 def naming_system
   PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
-  #PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
   PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   return PORTION_1
   
